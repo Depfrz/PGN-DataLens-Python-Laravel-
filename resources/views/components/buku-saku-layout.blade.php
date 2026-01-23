26b228311a29aed2ab2223aacd8d3f2f86efb22b
raffaanung <raffahaqqi1@gmail.com>
2026-01-23 13:22:48 +0700

    refactor(buku-saku): remove close button from notifications, rely on auto-dismiss

--- resources/views/components/buku-saku-layout.blade.php
+++ resources/views/components/buku-saku-layout.blade.php
@@ -109,19 +109,12 @@
                                             <strong class="font-bold">Berhasil!</strong>
                                             <span class="block sm:inline ml-1">{{ session('success') }}</span>
                                         </div>
-                                        <div class="flex-shrink-0 ml-4">
-                                            <button @click="show = false" class="text-green-700 hover:text-green-900 focus:outline-none">
-                                                <svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20">
-                                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                                                </svg>
-                                            </button>
-                                        </div>
                                     </div>
                                 </div>
                             @endif
 
                             @if(session('error'))
-                                <div x-data="{ show: true }" x-show="show" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative shadow-sm" role="alert">
+                                <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 5000)" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative shadow-sm" role="alert">
                                     <div class="flex items-start">
                                         <div class="flex-shrink-0">
                                             <svg class="h-5 w-5 text-red-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
@@ -131,13 +124,6 @@
                                         <div class="flex-1">
                                             <strong class="font-bold">Error!</strong>
                                             <span class="block sm:inline ml-1">{{ session('error') }}</span>
-                                        </div>
-                                        <div class="flex-shrink-0 ml-4">
-                                            <button @click="show = false" class="text-red-700 hover:text-red-900 focus:outline-none">
-                                                <svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20">
-                                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                                                </svg>
-                                            </button>
                                         </div>
                                     </div>
                                 </div>
